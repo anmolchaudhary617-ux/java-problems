300d7242a95a782bf349ab4221156e595e32ee46
Anmol <anmolchaudhary617@gmail.com>
2026-07-31 22:17:43 +0530

    Add uppercase and lowercase letter counter

---
+++ Strings/CountUpperLowerCase.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class CountUpperLowerCase {
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        /*
+         * Write a Java program that:
+         * Takes a string from the user
+         * Counts:
+         * number of uppercase letters
+         * number of lowercase letters
+         * Prints both counts
+         */
+        Scanner scan = new Scanner(System.in);
+
+        String str;
+
+        System.out.println("Enter a String(sentence):");
+        str = scan.nextLine();
+
+        int upperCount = 0, lowerCount = 0;
+
+        for(int i = 0; i < str.length(); i++){
+
+            char c = str.charAt(i);
+
+            if(c >= 'A' && c <='Z'){
+                upperCount++;
+            }
+            else if(c >= 'a' && c <='z'){
+                lowerCount++;
+            }
+        }
+
+        System.out.println("Number of UpperCase letters in the entered string: "+ upperCount);
+        System.out.println("Number of LowerCase letters in the entered string: "+ lowerCount);
+        scan.close();
+    }
+}
